83be6b706d6f786505642d98f52c191d27098959
bnannuri <bnannuri@gmail.com>
2020-06-02 13:26:30 -0400

    Adding new files for lambda function.

---
+++ transformWBInd.py
@@ -0,0 +1,37 @@
+import json
+import pandas as pd
+import os
+import boto3
+
+s3_bucketname = os.environ['s3_bucketname']	
+s3_ocha_transformed_key	= os.environ['s3_ocha_wbtransformed_key'] 
+temp_output_filename = os.environ['temp_output_filename']
+temp_output_filepath = os.environ['temp_output_filepath']	
+wb_feed_url = os.environ['wb_feed_url']
+
+def transform_handler(event, context):
+    transformIndicators()
+    write_to_s3()
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('WB Indicators file successfully transformed')
+    }
+
+def write_to_s3( ):
+   s3_client = boto3.client('s3')
+   object = s3_client.upload_file(temp_output_filepath+s3_ocha_transformed_key,s3_bucketname,s3_ocha_transformed_key)
+
+def transformIndicators():
+    df1 = pd.read_excel(wb_feed_url, 'Data', skiprows=3).set_index(['Country Name','Country Code'])
+    df1.drop(['Indicator Name','Indicator Code'], axis=1,inplace=True)
+    df2 = df1.T.unstack(level=1)
+    df2.to_csv(temp_output_filepath+temp_output_filename)
+    
+    # FIX ME : Since df2 is a series, couldn't override column names, so had to load the dataframe again.
+    df4 = pd.read_csv(temp_output_filepath+temp_output_filename, encoding='utf-8')
+    df4.columns =['Country Name', 'Country Code', 'Year', 'Population total']
+    df4.to_csv(temp_output_filepath+s3_ocha_transformed_key)
+    return
+
